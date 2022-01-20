2d58ac5a48419a55450d4f3903df2835d4b5a1ee
jennysun1001 <sunchenny17@kit.edu.kh>
2022-01-20 09:42:51 +0700

    Addon for hide button cancel entry

---
+++ account_restrict_cancel_journal/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_cancel_journal",
+    'summary': """Restrict User to cancel the journal """,
+    'description': """ Only user with Access for Cancel Journal can see the button 'Cancel' and 'Reset to draft for each invoice' """,
+    'author': "A2A Digital",
+    'category': 'account',
+    'version': '14.0.1.0.0',
+    'depends': ['account'],
+    'data': [
+        'views/account_cancel_journal_views.xml',
+    ],
+}
