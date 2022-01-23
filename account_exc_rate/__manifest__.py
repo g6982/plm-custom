94a595fed80004d85ad3d6fcc2d08c4c4f5895f7
jennysun1001 <sunchenny17@kit.edu.kh>
2022-01-23 23:51:34 +0700

    exchange rate only

---
+++ account_exc_rate/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_exc_rate",
+    'summary': """ Add 2 fields more for exchange rate in khmer and thai bath """,
+    'description': """
+        Exchange Rate in KHR and THB
+    """,
+    'author': "Jenny Sun ",
+    'category': 'Extra Tool',
+    'version': '14.0.1.0.0',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_exc_rate_views.xml',
+    ],
+    'demo': [],
+}
