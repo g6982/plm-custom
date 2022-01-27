fb14fcecc7218d67f10292e7185c3b68e21ef8b9
jennysun1001 <sunchenny17@kit.edu.kh>
2022-01-27 12:56:16 +0700

    take out require from driver name

--- driver_count/models/stock_picking.py
+++ driver_count/models/stock_picking.py
@@ -5,6 +5,6 @@ from odoo import models, fields, api
 class StockPicking(models.Model):
     _inherit = ['stock.picking']
 
-    driver_name = fields.Many2one('res.partner',string="Driver Name", store=True, randomly=True, required=True)
+    driver_name = fields.Many2one('res.partner',string="Driver Name", store=True, randomly=True,)
 
             
