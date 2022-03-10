6d2bd4ca7e5d713ebcb89eda989295d0f0638b0d
jenny <jennysun1001@gmail.com>
2022-03-10 09:28:15 +0700

    account invocie line

---
+++ account_invoice_line_report/models/account_move_line.py
@@ -0,0 +1,10 @@
+
+from odoo import models, fields, api,_
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.invoice.report"
+
+    label_name = fields.Char(string="Label Name")
+
+    def _select(self):
+        return super()._select() + ", line.name as label_name"
