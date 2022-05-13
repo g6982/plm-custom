61117f1f65872ff96f9d58e3f234a3f70c56107a
jenny <jennysun1001@gmail.com>
2022-05-13 15:25:00 +0700

    update invoice

---
+++ account_invoice_line_report/account_invoice_line_report/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Tecnativa - Carlos Dauden
+# Copyright 2018 Tecnativa - Vicent Cubells
+# Copyright 2020 Tecnativa - João Marques
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice Line Report",
+    "summary": "New view to manage invoice lines information",
+    "version": "14.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/OCA/account-invoice-reporting",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["account",],
+    "data": [
+        "reports/report.xml",
+        "reports/account_invoice_report_view.xml",
+        "reports/report_driver_expense.xml",
+        "reports/report_expense_statement.xml",
+        "reports/report_invoice_lines.xml",
+       
+        ],
+    "installable": True,
+}
