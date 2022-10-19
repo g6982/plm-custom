513d33a8c57f9c25f557d608e68eb4fd70f0ab51
Jenny Sun <jennysun1001@gmail.com>
2022-10-19 10:20:28 +0700

    modify invoice line for Unit Price and Subtotal without Tax

--- account_invoice_line_report/models/account_move_line.py
+++ account_invoice_line_report/models/account_move_line.py
@@ -6,7 +6,7 @@ class AccountMoveLine(models.Model):
     _inherit = "account.invoice.report"
 
     label_name = fields.Char(string="Label Name")
-    sub_pricetotal = fields.Float(string="Amount in Currency")
+    sub_pricetotal = fields.Float(string="Total Amount with Tax")
     price_unit_cus = fields.Float(string="Price Unit")
     currency_id_name = fields.Many2one('res.currency',string="Currency Type")
     amount_in_currency = fields.Monetary(currency_field="currency_id_name",string="Amount in Currency")
@@ -19,5 +19,6 @@ class AccountMoveLine(models.Model):
     thb_register_rate = fields.Float(string="THB Register Rate")
     inv_date = fields.Date(string="Inv or Bill Date")
     payment_refs = fields.Char(string="Memo/Respect Note")
+    
     def _select(self):
-        return super()._select() + " , move.payment_ref as payment_refs , move.invoice_date as inv_date , move.khr_rate_register as khr_register_rate , move.thb_rate_register as thb_register_rate , move.date_register as register_date , move.driver_names as driver , move.rate_khr as rate_khr_inh , move.rate_thb as rate_thb_inh , line.amount_currency as amount_in_currency, move.currency_id as move_currency_id, line.name as label_name , line.price_total as sub_pricetotal , line.price_unit as price_unit_cus , line.currency_id as currency_id_name " 
+        return super()._select() + " , move.payment_ref as payment_refs , move.invoice_date as inv_date , move.khr_rate_register as khr_register_rate , move.thb_rate_register as thb_register_rate , move.date_register as register_date , move.driver_names as driver , move.rate_khr as rate_khr_inh , move.rate_thb as rate_thb_inh , line.amount_currency as amount_in_currency, move.currency_id as move_currency_id, line.name as label_name , line.price_total as sub_pricetotal , line.price_unit as price_unit_cus , line.currency_id as currency_id_name" 
